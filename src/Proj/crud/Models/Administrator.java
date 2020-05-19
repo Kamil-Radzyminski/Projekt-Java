0e3c36d7410ae0240fde9642940d33fcb632c9a7
Kamil Radzymiński <radzyminski.kamil@gmail.com>
2020-05-19 19:14:51 +0200

    Zakończenie modelu User

---
+++ src/Proj/crud/Models/Administrator.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Proj.crud.Models;
+
+/**
+ *
+ * @author Kamil
+ */
+public class Administrator extends AbstractModel {
+
+    @Override
+    public boolean delete(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public AbstractModel create() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public AbstractModel update(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
--- src/Proj/crud/Models/User.java
+++ src/Proj/crud/Models/User.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Proj.crud.Models;
 
 import java.sql.ResultSet;
@@ -10,84 +5,233 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.PreparedStatement;
 
 /**
- *
+ * User model
  * @author Kamil
  */
 public class User extends AbstractModel {
 
-    private final String firstname;
-    private final String lastname;
-    private final String role;
-    private final String login;
-    private final String password;
-    private Integer id;
+    private static final String SQL_DELETE = "DELETE FROM uzytkownicy WHERE id=?";
+    private static final String SQL_GET = "SELECT * FROM uzytkownicy";
+    private static final String SQL_UPDATE = "UPDATE uzytkownicy SET sektor_id=?, nazwisko=?, imie=?, rola=?, login=?, haslo=? WHERE id=?";
+    private static final String SQL_INSERT = "INSERT INTO uzytkownicy(sektor_id, nazwisko, imie, rola, login, haslo) VALUES (?, ?, ?, ?, ?, ?)";
 
-    public User(String firstname, String lastname, String role, String login, String password) throws SQLException {
+    private String firstname;
+    private String lastname;
+    private String role;
+    private String login;
+    private String password;
+    private Integer id;
+    private Integer sectorId;
+
+    /**
+     * Create User
+     * @param firstname
+     * @param lastname
+     * @param role
+     * @param login
+     * @param password
+     * @param sectorId
+     * @throws SQLException 
+     */
+    public User(String firstname, String lastname, String role, String login, String password, int sectorId) throws SQLException {
         this.firstname = firstname;
         this.lastname = lastname;
         this.role = role;
         this.login = login;
         this.password = password;
+        this.sectorId = sectorId;
     }
 
-    public User(String firstname, String lastname, String role, String login, String password, Integer id) throws SQLException {
+    /**
+     * Create user with id
+     * @param firstname
+     * @param lastname
+     * @param role
+     * @param login
+     * @param password
+     * @param sectorId
+     * @param id
+     * @throws SQLException 
+     */
+    public User(String firstname, String lastname, String role, String login, String password, int sectorId, Integer id) throws SQLException {
         this.firstname = firstname;
         this.lastname = lastname;
         this.role = role;
         this.login = login;
         this.password = password;
+        this.sectorId = sectorId;
         this.id = id;
     }
-    
-    public String getFirstname(){
+
+    /**
+     * Set firstname
+     * @param firstname
+     * @return 
+     */
+    public User setFirstname(String firstname) {
+        this.firstname = firstname;
+        return this;
+    }
+
+    /**
+     * Set lastname
+     * @param lastname
+     * @return 
+     */
+    public User setLastname(String lastname) {
+        this.lastname = lastname;
+        return this;
+    }
+
+    /**
+     * Set role
+     * @param role
+     * @return 
+     */
+    public User setRole(String role) {
+        this.role = role;
+        return this;
+    }
+
+    /**
+     * Set login
+     * @param login
+     * @return 
+     */
+    public User setLogin(String login) {
+        this.login = login;
+        return this;
+    }
+
+    /**
+     * Set password
+     * @param password
+     * @return 
+     */
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    /**
+     * Set sectorId
+     * @param sectorId
+     * @return 
+     */
+    public User setSectorId(int sectorId) {
+        this.sectorId = sectorId;
+        return this;
+    }
+
+    /**
+     * Get firstname
+     * @return 
+     */
+    public String getFirstname() {
         return this.firstname;
     }
-    
-    public String getLastname(){
+
+    /**
+     * Get lastname
+     * @return 
+     */
+    public String getLastname() {
         return this.lastname;
     }
-    
-    public String getRole(){
+
+    /**
+     * Get role
+     * @return 
+     */
+    public String getRole() {
         return this.role;
     }
-    
-    public String getLogin(){
+
+    /**
+     * Get login
+     * @return 
+     */
+    public String getLogin() {
         return this.login;
     }
-    
-    public String getPassword(){
+
+    /**
+     * Get password
+     * @return 
+     */
+    public String getPassword() {
         return this.password;
     }
-    
-    public Integer getId(){
+
+    /**
+     * Get id
+     * @return 
+     */
+    public Integer getId() {
         return this.id;
     }
 
     @Override
-    public boolean delete(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean delete(Integer id) throws SQLException {
+        PreparedStatement preparedStatement = AbstractModel.getConnection().prepareStatement(SQL_DELETE);
+        preparedStatement.setInt(1, id);
+        preparedStatement.executeUpdate();
+        return true;
     }
 
     @Override
-    public AbstractModel create() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public AbstractModel create() throws SQLException {
+        PreparedStatement preparedStatement = AbstractModel.getConnection().prepareStatement(SQL_INSERT, Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setInt(0, this.sectorId);
+        preparedStatement.setString(1, this.lastname);
+        preparedStatement.setString(2, this.firstname);
+        preparedStatement.setString(3, this.role);
+        preparedStatement.setString(4, this.login);
+        preparedStatement.setString(5, this.password);
+        preparedStatement.executeUpdate();
+
+        ResultSet rs = preparedStatement.getGeneratedKeys();
+        if (rs.next()) {
+            this.id = rs.getInt(1);
+        }
+        
+        return this;
     }
 
     @Override
-    public AbstractModel update(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public AbstractModel update(Integer id) throws SQLException {
+        PreparedStatement preparedStatement = AbstractModel.getConnection().prepareStatement(SQL_UPDATE);
+        preparedStatement.setInt(0, this.sectorId);
+        preparedStatement.setString(1, this.lastname);
+        preparedStatement.setString(2, this.firstname);
+        preparedStatement.setString(3, this.role);
+        preparedStatement.setString(4, this.login);
+        preparedStatement.setString(5, this.password);
+        preparedStatement.setInt(6, this.id);
+        preparedStatement.executeUpdate();
+        return this;
     }
 
     public static List<User> getList() throws SQLException {
         List<User> usersList = new ArrayList<>();
         Statement stmt = AbstractModel.getConnection().createStatement();
-        String query = "select * from uzytkownicy";
-        ResultSet result = stmt.executeQuery(query);
+        ResultSet result = stmt.executeQuery(User.SQL_GET);
 
         while (result.next()) {
-            usersList.add(new User(result.getString("imie"), result.getString("nazwisko"), result.getString("rola"), result.getString("login"), result.getString("haslo"), result.getInt("id")));
+            usersList.add(
+                    new User(
+                            result.getString("imie"),
+                            result.getString("nazwisko"),
+                            result.getString("rola"),
+                            result.getString("login"),
+                            result.getString("haslo"),
+                            result.getInt("sektor_id"),
+                            result.getInt("id")
+                    )
+            );
         }
 
         return usersList;
